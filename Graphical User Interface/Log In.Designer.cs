9ba6e4cc01ca2a1d96f70755c0a8d3d5fbaab4de
Mharwin M. <callighster@icloud.com>
2024-05-19 17:58:34 +0800

    new login GUI design

---
+++ Graphical User Interface/Log In.Designer.cs
@@ -0,0 +1,121 @@
+﻿namespace App.GUI
+{
+    partial class Login
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges7 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Login));
+            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges8 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
+            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges9 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
+            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges10 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
+            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges11 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
+            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges12 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
+            TextBox_Enter_UsernameID = new Guna.UI2.WinForms.Guna2TextBox();
+            TextBox_Enter_Password = new Guna.UI2.WinForms.Guna2TextBox();
+            Button_Log_In = new Guna.UI2.WinForms.Guna2Button();
+            Label_CreateAccount = new Guna.UI2.WinForms.Guna2HtmlLabel();
+            SuspendLayout();
+            // 
+            // TextBox_Enter_UsernameID
+            // 
+            TextBox_Enter_UsernameID.CustomizableEdges = customizableEdges7;
+            TextBox_Enter_UsernameID.DefaultText = "";
+            TextBox_Enter_UsernameID.DisabledState.BorderColor = Color.FromArgb(208, 208, 208);
+            TextBox_Enter_UsernameID.DisabledState.FillColor = Color.FromArgb(226, 226, 226);
+            TextBox_Enter_UsernameID.DisabledState.ForeColor = Color.FromArgb(138, 138, 138);
+            TextBox_Enter_UsernameID.DisabledState.PlaceholderForeColor = Color.FromArgb(138, 138, 138);
+            TextBox_Enter_UsernameID.FocusedState.BorderColor = Color.FromArgb(94, 148, 255);
+            resources.ApplyResources(TextBox_Enter_UsernameID, "TextBox_Enter_UsernameID");
+            TextBox_Enter_UsernameID.HoverState.BorderColor = Color.FromArgb(94, 148, 255);
+            TextBox_Enter_UsernameID.Name = "TextBox_Enter_UsernameID";
+            TextBox_Enter_UsernameID.PasswordChar = '\0';
+            TextBox_Enter_UsernameID.PlaceholderText = "";
+            TextBox_Enter_UsernameID.SelectedText = "";
+            TextBox_Enter_UsernameID.ShadowDecoration.CustomizableEdges = customizableEdges8;
+            // 
+            // TextBox_Enter_Password
+            // 
+            TextBox_Enter_Password.CustomizableEdges = customizableEdges9;
+            TextBox_Enter_Password.DefaultText = "";
+            TextBox_Enter_Password.DisabledState.BorderColor = Color.FromArgb(208, 208, 208);
+            TextBox_Enter_Password.DisabledState.FillColor = Color.FromArgb(226, 226, 226);
+            TextBox_Enter_Password.DisabledState.ForeColor = Color.FromArgb(138, 138, 138);
+            TextBox_Enter_Password.DisabledState.PlaceholderForeColor = Color.FromArgb(138, 138, 138);
+            TextBox_Enter_Password.FocusedState.BorderColor = Color.FromArgb(94, 148, 255);
+            resources.ApplyResources(TextBox_Enter_Password, "TextBox_Enter_Password");
+            TextBox_Enter_Password.HoverState.BorderColor = Color.FromArgb(94, 148, 255);
+            TextBox_Enter_Password.Name = "TextBox_Enter_Password";
+            TextBox_Enter_Password.PasswordChar = '\0';
+            TextBox_Enter_Password.PlaceholderText = "";
+            TextBox_Enter_Password.SelectedText = "";
+            TextBox_Enter_Password.ShadowDecoration.CustomizableEdges = customizableEdges10;
+            // 
+            // Button_Log_In
+            // 
+            Button_Log_In.BorderRadius = 25;
+            Button_Log_In.CustomizableEdges = customizableEdges11;
+            Button_Log_In.DisabledState.BorderColor = Color.DarkGray;
+            Button_Log_In.DisabledState.CustomBorderColor = Color.DarkGray;
+            Button_Log_In.DisabledState.FillColor = Color.FromArgb(169, 169, 169);
+            Button_Log_In.DisabledState.ForeColor = Color.FromArgb(141, 141, 141);
+            Button_Log_In.FillColor = Color.FromArgb(29, 73, 167);
+            resources.ApplyResources(Button_Log_In, "Button_Log_In");
+            Button_Log_In.ForeColor = Color.White;
+            Button_Log_In.Name = "Button_Log_In";
+            Button_Log_In.ShadowDecoration.CustomizableEdges = customizableEdges12;
+            // 
+            // Label_CreateAccount
+            // 
+            Label_CreateAccount.BackColor = Color.Transparent;
+            resources.ApplyResources(Label_CreateAccount, "Label_CreateAccount");
+            Label_CreateAccount.ForeColor = Color.FromArgb(29, 73, 167);
+            Label_CreateAccount.Name = "Label_CreateAccount";
+            Label_CreateAccount.Click += Label_Sign_Up_Click;
+            // 
+            // Login
+            // 
+            resources.ApplyResources(this, "$this");
+            AutoScaleMode = AutoScaleMode.Font;
+            Controls.Add(Label_CreateAccount);
+            Controls.Add(Button_Log_In);
+            Controls.Add(TextBox_Enter_Password);
+            Controls.Add(TextBox_Enter_UsernameID);
+            DoubleBuffered = true;
+            Name = "Login";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Guna.UI2.WinForms.Guna2TextBox TextBox_Enter_UsernameID;
+        private Guna.UI2.WinForms.Guna2TextBox TextBox_Enter_Password;
+        private Guna.UI2.WinForms.Guna2Button Button_Log_In;
+        private Guna.UI2.WinForms.Guna2HtmlLabel Label_CreateAccount;
+    }
+}
--- Graphical User Interface/Log In.cs
+++ Graphical User Interface/Log In.cs
@@ -7,20 +7,20 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
-using Windows.Services.Maps.LocalSearch;
-using App.Models;
 
-namespace App.GUI {
-    public partial class Login : Form {
+namespace App.GUI
+{
+    public partial class Login : Form
+    {
         private readonly Client _client;
-
-        public Login(Client client) {
+        public Login(Client client)
+        {
             _client = client;
             InitializeComponent();
         }
-
+        
         private void Trigger_Shown(object sender, EventArgs e) {
-            Console.WriteLine("PROGRAM [GUI] RENDERED LOGIN UI");
+            Console.WriteLine("APPLICATION [GUI] RENDERED LOGIN UI");
 
             if (_client.User.IsLoggedIn) {
                 Console.WriteLine("    " + $"LOGIN: ALREADY LOGGED IN {_client.User.Account?.Username}");
@@ -29,11 +29,11 @@ namespace App.GUI {
             }
         }
 
-        private void Button_LOGIN_Click(object sender, EventArgs e) {
-            string username = CTRL_TextBox_Enter_Username.Text;
-            string password = CTRL_TextBox_Enter_Password.Text;
+        private void Button_Log_In_Click(object sender, EventArgs e) {
+            string username = TextBox_Enter_UsernameID.Text;
+            string password = TextBox_Enter_Password.Text;
 
-            Console.WriteLine($"[EVENT] ATTEMPT LOGIN");
+            Console.WriteLine($"PROGRAM [EVENT] ATTEMPT LOGIN");
             Console.WriteLine("    " + $"LOGIN: TRYING {username}:{password}");
 
             bool login = _client.User.Login(username, password);
@@ -41,10 +41,15 @@ namespace App.GUI {
                 Console.WriteLine("    " + $"LOGIN: LOGGED IN {_client.User.Account?.Username}");
                 this.Hide();
                 _client.UI.Home.Show();
-                
             }
-            else
-                Console.WriteLine("    " + $"LOGIN: FAILED {username}");
+            else Console.WriteLine("    " + $"LOGIN: FAILED {username}");
+        }
+        
+        private void Label_Sign_Up_Click(object sender, EventArgs e) {
+            Console.WriteLine($"APPLICATION [EVENT] REDIRECT SIGN UP");
+            
+            this.Hide();
+            _client.UI.SignUp.Show();
         }
     }
 }
